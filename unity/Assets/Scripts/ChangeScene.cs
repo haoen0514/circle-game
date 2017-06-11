3b0fb0d3dd4b433177d9700c95da051dfc30e8f6
brian <b02902096@ntu.edu.tw>
2017-06-11 22:54:03 +0800

    change scene

---
+++ unity/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("hi");
+	}
+	void OnMouseDown (){
+		Debug.Log ("hi");
+		Application.LoadLevel("Scenes/V2");
+	}
+}
