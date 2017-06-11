baaa40c6c19c68b6c149224f18bc851d4e764bfa
吳育騫 <andrew90622@gmail.com>
2017-06-11 21:34:15 +0800

    fix the code for maintain

--- unity/Assets/Scripts/BulletScript.cs
+++ unity/Assets/Scripts/BulletScript.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
 
 public class BulletScript : MonoBehaviour {
 	private Object ps;
@@ -31,13 +33,16 @@ public class BulletScript : MonoBehaviour {
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other){
-		Debug.Log (other);
+//		Regex
+//		Debug.Log (other);
 		if (index == 1) {
-			if (other.name == "16enemy(Clone)"|| other.name == "16enemy (1)(Clone)"|| other.name == "16enemy (2)(Clone)"|| other.name == "16enemy (3)(Clone)"|| other.name == "16enemy (4)(Clone)"|| other.name == "16enemy (5)(Clone)"|| other.name == "16enemy (6)(Clone)"|| other.name == "16enemy (7)(Clone)") {
+			if( Regex.IsMatch(other.name, "16enemy", RegexOptions.IgnoreCase) )
+			{
 				Destroy (this.gameObject);
 			}
 		} else {
-			if (other.name == "8enemy(Clone)" || other.name == "8enemy (1)(Clone)" || other.name == "8enemy (2)(Clone)" || other.name == "8enemy (3)(Clone)" || other.name == "8enemy (4)(Clone)" || other.name == "8enemy (5)(Clone)" || other.name == "8enemy (6)(Clone)" || other.name == "8enemy (7)(Clone)") {
+			if( Regex.IsMatch(other.name, "8enemy", RegexOptions.IgnoreCase) )
+			{
 				Destroy (this.gameObject);
 			}
 		}
